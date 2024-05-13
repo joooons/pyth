6b6e9a039c53f37a1a8a7c28f3b889cbffa4467a
joooons <joon.s.chang@gmail.com>
2024-05-13 15:55:26 -0700

    selection_sort()

--- sort.py
+++ sort.py
@@ -8,6 +8,18 @@ def array_of_int(min, max, n):
     return arr
 
 
+def selection_sort(arr):
+    n = len(arr)
+    # 1 5 4 2 3
+    for i in range(n-1):
+        i_min = i
+        for j in range(i+1, n):
+            if (arr[j] < arr[i_min]):
+                i_min = j
+        [arr[i_min], arr[i]] = [arr[i], arr[i_min]]
+    return arr
+
+
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
@@ -16,8 +28,14 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
-arr = array_of_int(0, 9, 12)
-print(arr)
+def main():
+    arr = array_of_int(0, 9, 5)
+    print(arr)
 
-bubble_sort(arr)
-print(arr)
+    selection_sort(arr)
+    bubble_sort(arr)
+    print(arr)
+
+
+if __name__ == "__main__":
+    main()
