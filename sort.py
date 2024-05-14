eab87a0029d3bc4fb3ec3aba448187a61b58d2a8
joooons <joon.s.chang@gmail.com>
2024-05-13 21:53:45 -0700

    refactoring

--- sort.py
+++ sort.py
@@ -59,58 +59,51 @@ def insertion_sort(arr):
 
 @show_time_duration
 def merge_sort(arr):
+    res = arr[:]
+
     def merge(arr):
         n = len(arr)
-        res = arr[:]
         if n <= 1:
-            return res
-        arA = merge(res[:n//2])
-        arB = merge(res[n//2:])
-        # print(f'merge {arA} :: {arB}')
+            return arr
+        arA = merge(arr[:n//2])
+        arB = merge(arr[n//2:])
         [a, b] = [0, 0]
         for i in range(n):
             if a == len(arA):
-                res[i] = arB[b]
+                arr[i] = arB[b]
                 b += 1
             elif b == len(arB):
-                res[i] = arA[a]
+                arr[i] = arA[a]
                 a += 1
             elif (arA[a] < arB[b]):
-                res[i] = arA[a]
+                arr[i] = arA[a]
                 a += 1
             else:
-                res[i] = arB[b]
+                arr[i] = arB[b]
                 b += 1
-        return res
-    return merge(arr)
+        return arr
+    return merge(res)
 
 
 @show_time_duration
 def custom_sort(arr):
     n = len(arr)
     res = arr[:]
-
     return res
 
 
 def main():
-    arr = array_of_int(0, 9, 7)
-    print(arr)
-    # a = [1]
-    # b = [1, 5]
-    # c = [1, 5, 2]
-    # d = [1, 5, 2, 6]
+    arr = array_of_int(0, 9, 9)
 
-    # ss_arr = selection_sort(arr)
-    # bs_arr = bubble_sort(arr)
-    # is_arr = insertion_sort(arr)
-    # ms_arr = merge_sort(a)
-    # ms_arr = merge_sort(b)
-    # ms_arr = merge_sort(c)
-    # ms_arr = merge_sort(d)
+    ss_arr = selection_sort(arr)
+    bs_arr = bubble_sort(arr)
+    is_arr = insertion_sort(arr)
     ms_arr = merge_sort(arr)
-    print(ms_arr)
-    # cs_arr = custom_sort(arr)
+    cs_arr = custom_sort(arr)
+
+    if len(arr) < 10:
+        print(arr)
+        print(cs_arr)
 
 
 if __name__ == "__main__":
